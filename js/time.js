231dceaddbf7ea82c70422428ca62db41a420fc6
fantom030983 <ihorkavun1234@gmail.com>
2023-09-27 14:42:35 +0300

    Homework Time

---
+++ js/time.js
@@ -0,0 +1,10 @@
+var time = prompt('Кількість годин?', ' ');
+
+var secondInAMinute = 60;
+var minutesInHour = 60;
+var secondInAHour = secondInAMinute * minutesInHour;
+alert("Кількість секунд: " + secondInAHour * time);
+
+
+
+
